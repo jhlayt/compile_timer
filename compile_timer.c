43e935ff98745a804735dc0664f5c377eee24c5d
jack-op67 <mail@jhlayton.com>
2025-12-26 21:21:33 +0000

    Updated copyright.

--- compile_timer.c
+++ compile_timer.c
@@ -1,7 +1,7 @@
 /* ============================================================================
 MIT License
 
-Copyright (c) 2025 gzop67
+Copyright (c) 2025 jhlayt
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
